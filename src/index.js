12df4d6eedd7e43030cf2041964a90a1d0188af6
LudMono <theblamno@gmail.com>
2022-03-25 11:05:09 +0300

    feat: add basic structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('sss');
+import "regenerator-runtime/runtime";
+
+import { endPoints } from "../utils/constants";
+import GetDataApi from "../utils/axios/GetDataApi";
+
+const getDataApi = new GetDataApi();
+
+(async () => {
+  const response = await getDataApi.getData(endPoints.comics);
+  console.log(response.data.data.results);
+})();
